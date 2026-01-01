edfe756aac26b235d1ead0fd52fb2cce9b3fb186
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-02 02:58:25 +0300

    Remove unused dependencies and cleanup install scripts
    Removed several packages from requirements.txt and eliminated redundant pip install commands from shell scripts. Deleted install_req.sh and updated web_venv.sh to delegate installation to pip.sh, streamlining the dependency management process.

--- web_sh/pip.sh
+++ web_sh/pip.sh
@@ -8,8 +8,5 @@ source $HOME/www/python/venv/bin/activate
 
 pip install pip -U
 pip install -r $HOME/www/python/src/requirements.txt
-pip install flask flask_cors tqdm psutil humanize
-
-pip install ArWikiCatsWeb -U
 
 # toolforge-jobs run pipup --image python3.11 --command ~/web_sh/pip.sh --wait
